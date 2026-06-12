34fb25b33c22fba6fcde537b22161b689601bca4
mennaali02 <mennatullahali02@gmail.com>
2026-06-13 01:27:31 +0300

    seo edits

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://my-portfolio-steel-eight-18.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+} //This tells Google all your important pages and how often they change, helping with SEO and ensuring your site gets indexed properly.
